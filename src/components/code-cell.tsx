951360cd1f8caba6c9d58b51572132996017e6b6
Ted Semashov <tsemashov@amplify.com>
2022-02-13 19:42:43 +0200

    added redux, immer etc.

---
+++ src/components/code-cell.tsx
@@ -0,0 +1,39 @@
+import { useState, useEffect } from 'react';
+import CodeEditor from './code-editor';
+import Preview from './preview';
+import bundle from '../bundler';
+import Resizable from './resizable';
+
+const CodeCell = () => {
+    const [input, setInput] = useState('');
+    const [code, setCode] = useState('');
+    const [error, setError] = useState('');
+
+    useEffect(() => {
+        const timer = setTimeout(async () => {
+            const output = await bundle(input);
+            setCode(output.code);
+            setError(output.err);
+        }, 750);
+
+        return () => {
+            clearTimeout(timer);
+        };
+    }, [input])
+
+    return (
+    <Resizable direction='vertical'>
+        <div style={{height: '100%', display: 'flex', flexDirection: 'row'}}>
+            <Resizable direction='horizontal'>
+                <CodeEditor
+                    initialValue=""
+                    onChange={(value) => setInput(value)}
+                />
+            </Resizable>
+            <Preview code={code} err={error} />
+        </div>
+    </Resizable>
+    )
+};
+
+export default CodeCell;
